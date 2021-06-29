55ecd11686a47ac72e95708eabbbc6dc350a593b
Laurent Labine <laurentlabine@gmail.com>
2021-06-29 10:10:42 +0200

    Removed unnecessary stuff

--- server.js
+++ server.js
@@ -5,15 +5,6 @@ const startSignaling = require('./public/src/signaling');
 
 const app = express();
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use('/public', express.static(process.cwd() + '/public'));
-
-app.get('/', (req, res) => {
-  console.log('Reached Main Route');
-  res.sendFile(process.cwd() + '/views/index.html');
-});
-
 const server = http.createServer(app);
 const port = process.env.PORT || 3000;
 
